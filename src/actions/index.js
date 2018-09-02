4baa3b613bec751147a480c30e9bf08387ef539f
Pedro Silva <pedro.rls93@gmail.com>
2018-09-02 15:30:14 -0300

    action to fetch posts from api

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+import FETCH_POSTS from '../actions/types';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=uniquekey123';
+
+export function fetchPosts() {
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+    return {
+        type: FETCH_POSTS,
+        payload: request
+    };
+}
---
+++ src/actions/types.js
@@ -0,0 +1,1 @@
+export const FECTH_POSTS = 'fetch_posts';
