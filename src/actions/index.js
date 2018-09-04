9c73bd3e145829e36e8c1ce98083fd128cd38bc8
Pedro Silva <pedro.rls93@gmail.com>
2018-09-03 23:24:31 -0300

    removed unecessary consolo logs

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
-import { FETCH_POSTS } from '../actions/tsypes';
+import { FETCH_POSTS } from '../actions/types';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=123';
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-    console.log(request);
     return {
         type: FETCH_POSTS,
         payload: request
