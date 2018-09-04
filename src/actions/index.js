96edb4e1ff6c9690e6c655a7512612513547abb4
Pedro Silva <pedro.rls93@gmail.com>
2018-09-04 17:37:08 -0300

    action to request the creation of a new post

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { FETCH_POSTS } from '../actions/types';
+import { FETCH_POSTS, CREATE_POST } from '../actions/types';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=123';
@@ -9,6 +9,15 @@ export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
     return {
         type: FETCH_POSTS,
+        payload: request
+    };
+}
+
+export function createPost(values, callback) {
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values).
+        then(() => callback());
+    return {
+        type: CREATE_POST,
         payload: request
     };
 }
