de2e7cb7976fc781a1ab41adb437728f83b9f2ca
Pedro Silva <pedro.rls93@gmail.com>
2018-09-03 22:49:44 -0300

    api url fixed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
-import FETCH_POSTS from '../actions/types';
+import { FETCH_POSTS } from '../actions/tsypes';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=uniquekey123';
+const API_KEY = '?key=123';
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+    console.log(request);
     return {
         type: FETCH_POSTS,
         payload: request
